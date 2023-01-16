faa1836be5d0d86761fb830edc98988115a153a8
Filipe Daineze <filipe.souza@digisystem.com.br>
2023-01-16 04:56:08 -0300

    force

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb74c45b4b291f154d13cd62f96c8e1b.js"
+  "/precache-manifest.f0f160c3537917e035773ef1a66aa602.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-light-bootstrap-dashboard"});
