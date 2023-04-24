2e641dd5db51402c29be3b7efb88e4275f831c5e
Filipe Daineze <filipe.souza@digisystem.com.br>
2023-04-24 11:16:37 -0300

    GT3fix

---
+++ precache-manifest.c06f337d46c09e0cedb2cc6196dba4a7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "078242c4d29e6eba4f8e",
+    "url": "/endurance/css/app.923a676e.css"
+  },
+  {
+    "revision": "078242c4d29e6eba4f8e",
+    "url": "/endurance/js/app.e9176e52.js"
+  },
+  {
+    "revision": "6f2ef614e03415c09193",
+    "url": "/endurance/js/chunk-2d219ff9.1189c811.js"
+  },
+  {
+    "revision": "027fbcfb3d9ba2be7af0",
+    "url": "/endurance/css/chunk-vendors.f63cc037.css"
+  },
+  {
+    "revision": "027fbcfb3d9ba2be7af0",
+    "url": "/endurance/js/chunk-vendors.ced4bfee.js"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/endurance/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/endurance/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/endurance/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/endurance/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "3f5154a03aacab4217b01b4c7328fdd3",
+    "url": "/endurance/img/background.3f5154a0.jpg"
+  },
+  {
+    "revision": "3290078f583bd2defff2e4567e005afc",
+    "url": "/endurance/index.html"
+  },
+  {
+    "revision": "fb661d5480cd489ae7418406c12c96fc",
+    "url": "/endurance/android-chrome-192x192.png"
+  },
+  {
+    "revision": "d025c69a9475499064827b65b0de5637",
+    "url": "/endurance/apple-touch-icon.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/endurance/.gitkeep"
+  },
+  {
+    "revision": "b3f157a2c6392acdc25dea3138734971",
+    "url": "/endurance/android-chrome-512x512.png"
+  },
+  {
+    "revision": "cf1a2198b8563f6d78daa1c270941656",
+    "url": "/endurance/favicon-16x16.png"
+  },
+  {
+    "revision": "808bdc36bc40b9cbb3c25832d9d42b19",
+    "url": "/endurance/favicon-32x32.png"
+  },
+  {
+    "revision": "808bdc36bc40b9cbb3c25832d9d42b19",
+    "url": "/endurance/favicon.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/endurance/img/default-avatar.png"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/endurance/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "07c7a62e93cf404e0e3ec2db2233b5f1",
+    "url": "/endurance/README.md"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/endurance/site.webmanifest"
+  }
+];
